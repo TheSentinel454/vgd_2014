49dec2c7980ecf4203311900eca629c12062590d
Luke Tornquist <Luke.Tornquist@gmail.com>
2014-06-11 18:56:44 -0400

    Add initial projects

---
+++ Milestone 2/Milestone2/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelector : MonoBehaviour
+{
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	void Update()
+	{
+		// Air Garden
+		if (Input.GetKeyDown ("1"))
+		{
+			if (!"AirGarden".Equals(Application.loadedLevelName))
+				Application.LoadLevel ("AirGarden");
+		}
+		// Fire Garden
+		else if (Input.GetKeyDown ("2"))
+		{
+			if (!"FireGarden".Equals(Application.loadedLevelName))
+				Application.LoadLevel("FireGarden");
+		}
+		// Water Garden
+		else if (Input.GetKeyDown ("3"))
+		{
+			if (!"WaterGarden".Equals(Application.loadedLevelName))
+				Application.LoadLevel("WaterGarden");
+		}
+	}
+}
