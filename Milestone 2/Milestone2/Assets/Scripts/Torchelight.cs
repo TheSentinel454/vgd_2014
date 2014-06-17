be829ece0e865106fd738290660746d9c48dd059
Luke Tornquist <Luke.Tornquist@gmail.com>
2014-06-17 11:43:58 -0400

    Checkin latest air garden

--- Milestone 2/Milestone2/Assets/Scripts/Torchelight.cs
+++ Milestone 2/Milestone2/Assets/Scripts/Torchelight.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class Torchelight : MonoBehaviour {
-	
+public class Torchelight : MonoBehaviour
+{
 	public GameObject TorchLight;
 	public GameObject MainFlame;
 	public GameObject BaseFlame;
@@ -10,28 +10,61 @@ public class Torchelight : MonoBehaviour {
 	public GameObject Fumee;
 	public float MaxLightIntensity;
 	public float IntensityLight;
-	
 
-	void Start () {
-		TorchLight.light.intensity=IntensityLight;
-		MainFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*20f;
-		BaseFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*15f;	
-		Etincelles.GetComponent<ParticleSystem>().emissionRate=IntensityLight*7f;
-		Fumee.GetComponent<ParticleSystem>().emissionRate=IntensityLight*12f;
+	private ParticleSystem mainFlame;
+	private ParticleSystem baseFlame;
+	private ParticleSystem etincelles;
+	private ParticleSystem fumee;
+
+	void Start ()
+	{
+		TorchLight.light.intensity = IntensityLight;
+
+		mainFlame = MainFlame.GetComponent<ParticleSystem> ();
+		mainFlame.emissionRate = IntensityLight * 20f;
+
+		baseFlame = BaseFlame.GetComponent<ParticleSystem> ();
+		baseFlame.emissionRate = IntensityLight * 15f;
+
+		etincelles = Etincelles.GetComponent<ParticleSystem>();
+		etincelles.emissionRate = IntensityLight * 7f;
+
+		fumee = Fumee.GetComponent<ParticleSystem> ();
+		fumee.emissionRate = IntensityLight * 12f;
 	}
 	
 
-	void Update () {
-		if (IntensityLight<0) IntensityLight=0;
-		if (IntensityLight>MaxLightIntensity) IntensityLight=MaxLightIntensity;		
+	void Update ()
+	{
+		if (IntensityLight < 0)
+		{
+			IntensityLight = 0;
+		}
+		if (IntensityLight > MaxLightIntensity)
+		{
+			IntensityLight = MaxLightIntensity;
+		}
 
-		TorchLight.light.intensity=IntensityLight/2f+Mathf.Lerp(IntensityLight-0.1f,IntensityLight+0.1f,Mathf.Cos(Time.time*30));
+		TorchLight.light.intensity = IntensityLight/2f+Mathf.Lerp(IntensityLight-0.1f,IntensityLight+0.1f,Mathf.Cos(Time.time*30));
 
 		TorchLight.light.color=new Color(Mathf.Min(IntensityLight/1.5f,1f),Mathf.Min(IntensityLight/2f,1f),0f);
-		MainFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*20f;
-		BaseFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*15f;
-		Etincelles.GetComponent<ParticleSystem>().emissionRate=IntensityLight*7f;
-		Fumee.GetComponent<ParticleSystem>().emissionRate=IntensityLight*12f;		
+		mainFlame.emissionRate=IntensityLight*20f;
+		baseFlame.emissionRate=IntensityLight*15f;
+		etincelles.emissionRate=IntensityLight*7f;
+		fumee.emissionRate=IntensityLight*12f;
+	}
 
+	void OnTriggerStay(Collider other)
+	{
+		//Debug.Log ("OnTriggerStay: " + other.tag);
+		if (other.tag == "Player")
+		{
+			//Debug.Log ("Intensity: " + IntensityLight);
+			mainFlame.emissionRate -= 2.0f;
+			baseFlame.emissionRate -= 1.5f;
+			etincelles.emissionRate -= 0.7f;
+			fumee.emissionRate -= 1.2f;
+			IntensityLight -= 0.1f;
+		}
 	}
 }
---
+++ Milestone 2/Milestone2/Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+	void OnTriggerStay(Collider other)
+	{
+		Debug.Log ("OnTriggerStay: " + other.tag);
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("OnTriggerEnter: " + other.tag);
+	}
+}
