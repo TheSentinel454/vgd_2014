1b2a75cad75b85199103f58f1e2aa4183448de9b
Luke Tornquist <Luke.Tornquist@gmail.com>
2014-06-23 21:19:20 -0400

    Improve destroy by time

--- Project/NinjaMorph/Assets/Scripts/DestroyByTime.cs
+++ Project/NinjaMorph/Assets/Scripts/DestroyByTime.cs
@@ -3,10 +3,42 @@ using System.Collections;
 
 public class DestroyByTime : MonoBehaviour
 {
-	public float lifetime;
+	public float lifetime = float.MaxValue;
+
+	private ParticleSystem particles;
+	private float startTime;
+	private float endTime;
+	private float startRate;
 
 	void Start()
 	{
-		Destroy(gameObject, lifetime);
+		// Check for a particle system
+		particles = (ParticleSystem)GetComponent<ParticleSystem> ();
+		if (particles != null)
+		{
+			// Set start emission rate
+			startRate = particles.emissionRate;
+		}
+		// Set the times
+		startTime = Time.time;
+		endTime = startTime + lifetime;
+	}
+
+	void Update()
+	{
+		// Check for a particle system
+		if (particles != null)
+		{
+			// Set the emission rate based on the time passed
+			particles.emissionRate -= ((Time.deltaTime / (endTime - startTime)) * startRate) * 1.5f;
+			if (particles.emissionRate < 0.0f)
+				particles.emissionRate = 0.0f;
+		}
+		// Check for end condition
+		if (Time.time >= endTime)
+		{
+			// Destroy the object
+			Destroy(gameObject);
+		}
 	}
 }
