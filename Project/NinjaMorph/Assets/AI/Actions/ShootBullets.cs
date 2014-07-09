25450e58aa1681a2259fe7c3f64ded25563870cb
Jonathan Yates <jonathantyates@gmail.com>
2014-07-08 20:12:00 -0400

    update shoot bullets to predictively aim

--- Project/NinjaMorph/Assets/AI/Actions/ShootBullets.cs
+++ Project/NinjaMorph/Assets/AI/Actions/ShootBullets.cs
@@ -5,21 +5,32 @@ using RAIN.Core;
 using RAIN.Action;
 
 [RAINAction]
-public class ShootBullets : RAINAction
-{
-	private GameObject player_;
-	private GameObject bulletModel_;
-	private GameObject bullet_;
+public class ShootBullets : RAINAction {
+
+	// Player and Bullet Game Objects
+	private GameObject player;
+	private GameObject bulletModel;
+	private GameObject bullet;
+
+	// Player Velocity
+	private Vector3 playerPreviousPosition;
+	private Vector3 playerDisplacement = Vector3.zero;
+	private Vector3 playerVelocity = Vector3.zero;
+	private Vector3 aimingAt;
+
+	// Timers
 	private float timeOfLastShot = 0.0f;
 
+	// Constructor
     public ShootBullets() {
         actionName = "ShootBullets";
     }
 
 	// Start the AI and find the back of the gun and the bullet model
     public override void Start(AI ai) {
-		player_ = ai.WorkingMemory.GetItem<GameObject>("ninja").transform.Find("Entity").gameObject;
-		bulletModel_ = ai.Body.GetComponentInChildren<Bullet>().gameObject;
+		bulletModel = ai.Body.GetComponentInChildren<Bullet> ().gameObject;
+		player = ai.WorkingMemory.GetItem<GameObject> ("ninja").transform.Find ("Entity").gameObject;
+		playerPreviousPosition = player.transform.position;
         base.Start(ai);
     }
 
@@ -28,18 +39,21 @@ public class ShootBullets : RAINAction
 		// Limit the number of shots per second
 		if (Time.time - timeOfLastShot >= 0.35) {
 			// Create the new bullet instance
-			bullet_ = MonoBehaviour.Instantiate (bulletModel_) as GameObject;
+			bullet = MonoBehaviour.Instantiate (bulletModel) as GameObject;
+			bullet.GetComponent<Bullet>().destroyable = true;
+			bullet.renderer.enabled = true;
 
-			// Make the bullet destroyable
-			bullet_.GetComponent<Bullet>().destroyable = true;
-			bullet_.renderer.enabled = true;
+			// Determine where we are aiming
+			playerVelocity = ai.WorkingMemory.GetItem<GameObject> ("ninja").GetComponent<NinjaController>().velocity;
 
-			// Position the bullet within the barrel
-			bullet_.transform.up = (player_.transform.position - bulletModel_.transform.position).normalized;
-			bullet_.transform.position = bulletModel_.transform.position;
+			// Aim at the player
+			float scalar = playerVelocity.magnitude * 0.5f;
+			aimingAt = (player.transform.position + player.transform.forward * scalar) - bulletModel.transform.position;
 
-			// FIRE!
-			bullet_.GetComponent<Bullet>().direction = (player_.transform.position - bulletModel_.transform.position).normalized;
+			// Position the bullet within the barrel and shoot
+			bullet.transform.up = aimingAt.normalized;
+			bullet.transform.position = bulletModel.transform.position;
+			bullet.GetComponent<Bullet>().direction = aimingAt.normalized;
 
 			// Update our time of last shot
 			timeOfLastShot = Time.time;
