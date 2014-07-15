d0d2e77f09be056a7ba1eaa5995660da5eebc9eb
Luke Tornquist <Luke.Tornquist@gmail.com>
2014-07-14 22:11:00 -0400

    Add Menu scene

---
+++ Project/NinjaMorph/Assets/Scripts/MenuMouseHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMouseHandler : MonoBehaviour
+{
+	/// <summary>
+	/// Raises the mouse down event.
+	/// </summary>
+	void OnMouseDown()
+	{
+		print ("MouseTrigger: " + this.name);
+		if (this.name == "PlayButton")
+		{
+			// Load the air level
+			Application.LoadLevel("airLevel");
+		}
+		else if (this.name == "HelpButton")
+		{
+			iTween.RotateTo(Camera.main.gameObject, new Vector3(0, 90, 0), 1.0f);
+		}
+		else if (this.name == "BackButton")
+		{
+			iTween.RotateTo(Camera.main.gameObject, new Vector3(0, 0, 0), 1.0f);
+		}
+	}
+}
