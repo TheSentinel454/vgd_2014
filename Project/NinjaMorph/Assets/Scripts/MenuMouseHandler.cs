fc2def517cd2c3cb2c9a66ec4f9c431371378116
Luke Tornquist <Luke.Tornquist@gmail.com>
2014-07-14 23:09:29 -0400

    Fix the level load to load the correct level

--- Project/NinjaMorph/Assets/Scripts/MenuMouseHandler.cs
+++ Project/NinjaMorph/Assets/Scripts/MenuMouseHandler.cs
@@ -15,28 +15,35 @@ public class MenuMouseHandler : MonoBehaviour
 		if (this.name == "PlayButton")
 		{
 			// Load the air level
-			Application.LoadLevel("airLevel");
+			Application.LoadLevel("AirRoom");
 		}
 		else if (this.name == "HelpButton")
 		{
+			// Rotate towards the help page
 			iTween.RotateTo(Camera.main.gameObject, new Vector3(0, 90, 0), rotateTransitionSpeed);
 		}
 		else if (this.name == "BackButton")
 		{
+			// Rotate back towards the main menu
 			iTween.RotateTo(Camera.main.gameObject, new Vector3(0, 0, 0), rotateTransitionSpeed);
 		}
 	}
 	
 	/// <summary>
-	/// Raises the mouse over event.
+	/// Raises the mouse enter event.
 	/// </summary>
 	void OnMouseEnter()
 	{
+		// Transition to gray
 		iTween.ColorTo (gameObject, Color.gray, colorTransitionSpeed);
 	}
-	
+
+	/// <summary>
+	/// Raises the mouse exit event.
+	/// </summary>
 	void OnMouseExit()
 	{
+		// Transition back to white
 		iTween.ColorTo (gameObject, Color.white, colorTransitionSpeed);
 	}
 }
