88252e454fb9d10ede9684d5c6fd5851abde0ad8
Jonathan Yates <jonathantyates@gmail.com>
2014-07-15 19:05:10 -0400

    ai animations and fire room

---
+++ Project/NinjaMorph/Assets/Cyborg Troop/Scripts/CyborgAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CyborgAnimationController : MonoBehaviour {
+
+	public Animator animator;
+
+	private Vector3 previousPosition;
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 hVelocity = Vector3.zero;
+	private Vector3 vVelocity = Vector3.zero;
+
+	void Start () {
+		previousPosition = transform.position;
+	}
+
+	void FixedUpdate () {
+		velocity = (transform.position - previousPosition) / Time.fixedDeltaTime;
+		previousPosition = transform.position;
+
+//		animator.SetFloat("speed", Mathf.Min(new Vector2(velocity.x, velocity.z).magnitude, 0.51f));
+	}
+}
---
+++ Project/NinjaMorph/Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float damage = 5.0f;
+
+	void OnCollisionEnter (Collision collision) {
+//		Destroy (gameObject);
+	}
+}
