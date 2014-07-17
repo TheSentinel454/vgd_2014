dbf046a9612b04b21618891257c94c1b4d985700
Jonathan Yates <jonathantyates@gmail.com>
2014-07-17 00:23:21 -0400

    fade out and remove cyborg troop on death

--- Project/NinjaMorph/Assets/CyborgTroopDeath.cs
+++ Project/NinjaMorph/Assets/CyborgTroopDeath.cs
@@ -6,5 +6,20 @@ public class CyborgTroopDeath : MonoBehaviour {
 
 	void ObjectDeath () {
 		transform.Find ("AI").GetComponent<AIRig> ().AI.WorkingMemory.SetItem<bool> ("Dead", true);
+
+		Renderer[] renderers = GetComponentsInChildren<Renderer> ();
+		foreach (Renderer childRenderer in renderers) {
+			childRenderer.material.shader = Shader.Find ("Transparent/Bumped Specular");
+		}
+
+		Hashtable fadeSettings = new Hashtable ();
+		fadeSettings.Add ("alpha", 0);
+		fadeSettings.Add ("time", 3);
+		fadeSettings.Add ("onComplete", "DestroyCyborgTroop");
+		iTween.FadeTo(gameObject, fadeSettings);
+	}
+
+	void DestroyCyborgTroop () {
+		DestroyObject (gameObject);
 	}
 }
