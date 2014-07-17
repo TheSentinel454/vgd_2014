3b5b3fecd65074c7d4a16e2b35a85bca962e0b6b
Jonathan Yates <jonathantyates@gmail.com>
2014-07-16 23:44:01 -0400

    Death from AI

---
+++ Project/NinjaMorph/Assets/CyborgTroopDeath.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+
+public class CyborgTroopDeath : MonoBehaviour {
+
+	void ObjectDeath () {
+		transform.Find ("AI").GetComponent<AIRig> ().AI.WorkingMemory.SetItem<bool> ("Dead", true);
+	}
+}
