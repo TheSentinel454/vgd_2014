09bee36e3844b7d828e32a5c136287d62399123c
Evan <elahurd3@gatech.edu>
2014-07-30 00:06:34 -0400

    added ascending sounds for filling up buckets

--- Project/NinjaMorph/Assets/Scripts/FillableObject.cs
+++ Project/NinjaMorph/Assets/Scripts/FillableObject.cs
@@ -1,4 +1,4 @@
-﻿﻿/*
+﻿/*
   Team Y-Not
   
   Evan LaHurd
@@ -11,11 +11,20 @@ using System.Collections;
 public class FillableObject : InteractiveObject
 {
 	public bool filled = false;
+
 	private Vector3 originalPosition = new Vector3(0,0,0);
 	private float fillAmount = 0.0f;
+	private bool didPlay; //did the note play already for the given puzzle attempt
+	private WaterSounds waterSounds;
 
 	void Start() {
 		originalPosition = transform.parent.FindChild("bucket_water").transform.position;
+		waterSounds = GameObject.Find ("Water Sounds").GetComponent<WaterSounds> ();
+		didPlay = false;
+	}
+
+	void Update() {
+
 	}
 
 	public override void PlayerCollisionStay(InteractiveCollision col) {
@@ -24,13 +33,27 @@ public class FillableObject : InteractiveObject
 			transform.parent.FindChild("bucket_water").Translate(0,0.03f,0);
 			fillAmount += 0.03f;
 		}
-		if (fillAmount >= 1.0) {
+		if (fillAmount >= 1.0 && !didPlay) {
 			filled = true;
+			waterSounds.playNextWaterSound();
+			didPlay = true;
 		}
 	}
 
 	public void clearBucket() {
 		transform.parent.FindChild("bucket_water").transform.position = originalPosition;
 		fillAmount = 0.0f;
+		didPlay = false;
+		waterSounds.getQueue().Clear();
+		waterSounds.fillQueue ();
 	}
+
+	public void setDidPlay(bool didPlay) {
+		this.didPlay = didPlay;
+	}
+
+	public bool getDidPlay() {
+		return didPlay;
+	}
+
 }
---
+++ Project/NinjaMorph/Assets/Scripts/WaterSounds.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterSounds : MonoBehaviour {
+	
+	public AudioSource[] waterSounds; 
+
+	private Queue waterQueue;
+	private bool[] playedSounds;
+	private AudioSource mainAudio;
+
+	// Use this for initialization
+	void Start () {
+		playedSounds = new bool[] {false, false, false, false};
+		mainAudio = waterSounds[0];
+		waterQueue = new Queue ();
+		fillQueue ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Queue getQueue() {
+		return waterQueue;
+	}
+
+	public AudioSource getAudioSource() {
+		return mainAudio;
+	}
+
+	public void fillQueue() {
+		waterQueue.Enqueue (waterSounds [0]);
+		waterQueue.Enqueue (waterSounds [1]);
+		waterQueue.Enqueue (waterSounds [2]);
+		waterQueue.Enqueue (waterSounds [3]);
+	}
+
+	public void playNextWaterSound() {
+		mainAudio = (AudioSource)waterQueue.Dequeue ();
+		mainAudio.Play();
+	}
+}
